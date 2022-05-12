874d83ceef104c5e4a52af2d6ce0935342e6a434
Viktar Hrapau <Viktar.Hrapau@itechart-group.com>
2022-05-12 13:32:50 +0300

    Fix docker for service app4

--- client/src/main/java/com/hrapau/configuration/Database.java
+++ client/src/main/java/com/hrapau/configuration/Database.java
@@ -1,7 +1,6 @@
 package com.hrapau.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 public class Database {
